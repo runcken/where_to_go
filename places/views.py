95d7b9c393b6bc4ea9e69f6d42dcfcde32dd6d50
runcken <runcken@gmail.com>
2025-11-14 20:01:36 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,5 +1,40 @@
 from django.shortcuts import render
+from django.templatetags.static import static
+import json
 
 
 def show_main(request):
     return render(request, 'index.html')
+
+
+def index(request):
+    geojson_data = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.62, 55.793676]
+                },
+                "properties": {
+                    "title": "«Легенды Москвы",
+                    "placeId": "moscow_legends",
+                    "detailsUrl": static('places/moscow_legends.json')
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.64, 55.753676]
+                },
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": static('places/roofs24.json')
+                }
+            }
+        ]
+    }
+    return render(request, 'index.html', {'geojson_data': geojson_data})
