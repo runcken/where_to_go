2551b3d97e69177994df12f130eb0cbd48a2f618
runcken <runcken@gmail.com>
2025-11-18 18:56:14 +0300

    rename environment variable

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': env.str('DATABASE_NAME', str(BASE_DIR / 'db.sqlite3')),
+        'NAME': env.str('DATABASE_PATH', str(BASE_DIR / 'db.sqlite3')),
     }
 }
 
